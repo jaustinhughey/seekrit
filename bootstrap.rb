be835dfeb92aa61853dd067de91f3430ad32d8df
J. Austin Hughey <jaustinhughey@gmail.com>
2015-04-09 00:04:57 -0500

    don't access ENV directly, define constants instead

--- bootstrap.rb
+++ bootstrap.rb
@@ -6,17 +6,23 @@ require "redis"
 require "rack-flash"
 require "sinatra/param"
 require "json"
+require "virtus"
 require_relative "app.rb"
 
 # Designate runlevel
-unless ENV['RACK_ENV'] && ENV['RACK_ENV'].length > 0
-  ENV['RACK_ENV'] = "development"
+if ENV['RACK_ENV'] && ENV['RACK_ENV'].length > 0
+  RUNTIME_ENV = ENV['RACK_ENV']
+else
+  RUNTIME_ENV = "development"
 end
 
 # Get all stuff under lib recursively
 Dir.glob(File.join(FileUtils.pwd, 'lib', '**', '*.rb')).each { |f| require f }
 
+# ...and all the models too
+Dir.glob(File.join(FileUtils.pwd, 'models', '**', '*.rb')).each { |f| require f }
+
 # For dev/test usage of Pry
-unless ENV['RACK_ENV'] == ('production' || 'staging')
+if RUNTIME_ENV == ('development' || 'test')
   require "pry"
 end
